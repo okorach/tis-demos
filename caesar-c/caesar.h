f597d0f42aae70831218d5a003d468a274e70497
okorach <olivier.korach@gmail.com>
2022-09-16 17:24:55 +0200

    Fix malloc() size

--- caesar-c/caesar.h
+++ caesar-c/caesar.h
@@ -1,6 +1,9 @@
 #ifndef CAESAR_H
 #define CAESAR_H
 
+// Max size of encrypted buffers
+#define MAX_BUF 38
+
 /* Ciphers the input string using the provided shift */
 
 char *caesar_encrypt(char *str, int shift);
